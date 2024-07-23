b79733c0a301d0ea38d7892f12e3e7bd5338b8cb
Allan Segovia Spadini <allan.spadini@gmail.com>
2024-07-23 09:43:46 -0300

    arquivos

---
+++ EcoBike.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+def main():
+    
+    st.set_page_config(
+        page_title="EcoBike",
+        page_icon="🚲",
+    )
+
+    st.markdown('''
+                ## Dashboard EcoBike: Acompanhe e Planeje o Sucesso do Seu Negócio 🚴‍♀️🚴‍♂️
+
+                Boas-vindas ao centro de controle da EcoBike! Aqui você encontra as ferramentas essenciais para impulsionar o seu serviço de aluguel de bicicletas. 🚲💨
+
+                **Explore as opções:**
+
+                * **Dashboard:** Mergulhe nos dados do seu negócio. 📊 Visualize o número de aluguéis, a receita gerada, os horários de pico e muito mais. Descubra insights valiosos para otimizar suas operações. 📈
+
+                * **Previsão de Aluguéis:** Planeje o futuro com confiança. 🔮 Nossa ferramenta de previsão estima a demanda por bicicletas nos próximos dias, ajudando você a ajustar a disponibilidade da frota e maximizar seus lucros. 🚀
+
+                **Importante:** Lembre-se que a precisão da previsão diminui à medida que aumenta o número de dias. 🗓️ Utilize as informações com sabedoria para tomar as melhores decisões para o seu negócio. 🤔
+
+                **Pronto para começar?** Clique na opção desejada e explore o poder dos dados com a EcoBike! 💪
+    ''')
+
+if __name__ == "__main__":
+    main()
---
+++ pages/2_📈_Previsor.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def main():
+   st.markdown('## Previsão de aluguéis')
+
+if __name__ == "__main__":
+    main()
