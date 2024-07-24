116ae1ec098baec186cf522680e0b115a47564c3
Allan Segovia Spadini <allan.spadini@gmail.com>
2024-07-24 09:55:43 -0300

    atu

--- pages/2_📈_Previsor.py
+++ pages/2_📈_Previsor.py
@@ -1,8 +1,28 @@
 import streamlit as st
+import joblib
 
+
+@st.cache_resource
+def carrega_modelo():
+      """
+      A function that loads a serialized machine learning model from a file.
+
+      Returns:
+         The loaded machine learning model.
+      """
+      return joblib.load('assets/modelo_prophet.pkl')
 
 def main():
-   st.markdown('## Previsão de aluguéis')
+      
+      model = carrega_modelo()
+      st.markdown('## Previsão de aluguéis')
+
+      n_dias = st.number_input('Quantos dias deseja prever?', min_value=1, max_value=365, value=1, step=1)
+      # Fazer previsões para datas futuras
+      future = model.make_future_dataframe(periods=n_dias)
+      forecast = model.predict(future)
+      fig = model.plot(forecast)
+      st.pyplot(fig)
 
 if __name__ == "__main__":
-    main()
+      main()
